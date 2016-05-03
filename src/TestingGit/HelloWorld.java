1e8b3ca1cc732f5ce69880ba629f04fa28a7c2a6
ReneDLC <renedlc1@gmail.com>
2016-05-03 14:36:36 -0500

    Added a counter

--- src/TestingGit/HelloWorld.java
+++ src/TestingGit/HelloWorld.java
@@ -15,7 +15,16 @@ public class HelloWorld {
 		
 	}
 	public static void main(String[] args){
-		System.out.println("Hi");
+		int counter = 0;
+		for(int i = 0; i<101;i++){
+			for(int j = -99; j<101;j++){
+				if(i == j*j){
+					counter++;
+					System.out.println(i + ":"+ j );
+				}
+			}	
+		}
+		//System.out.println(counter);
 	}
 	
 }
